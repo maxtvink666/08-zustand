deba8908449ff26ed6c2895170f85ace918a8466
torontotokyo <102730873+maxtvink666@users.noreply.github.com>
2026-04-01 01:27:14 +0300

    Update Open Graph URL for NoteHub

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "NoteHub",
     description: "Your digital space for thoughts and notes.",
-    url: "",
+    url: "https://08-zustand-mauve-delta.vercel.app/",
     siteName: "NoteHub",
     images: [
       {
